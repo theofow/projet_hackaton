bc59bd992aa070c6996fc1cefd96d212f519b57e
theophile <theophile.rabut@edu.devinci.fr>
2023-01-30 16:23:57 +0100

    ajout des classes battery robot et ressources, test de la création d'un agent

---
+++ projet.py
@@ -0,0 +1,62 @@
+#Ajout des lib
+from random import randint
+
+
+#############################################################
+#                         CLASSES                           #
+#############################################################
+
+
+
+class Position:
+    def __init__(self):
+        self.x = 0
+        self.y = 0
+
+    def coo(self):
+        return self.x, self.y
+    
+    def deplace(self, ax, ay):
+        self.x = self.x + ax
+        self.y = self.y +ay
+    
+    def reset_pos(self):
+        self.x = 0
+        self.y = 0
+        
+    def random_pos(self):
+        self.x = randint(0,255)
+        self.y = randint(0,255)
+    
+    
+class Battery:
+    def __init__(self):
+        self.percent = 100
+        self.level = 1
+        
+    def using_battery(self):
+        self.percent = self.percent - 1
+
+class Robot:
+    def __init__(self):
+        self.position = Position()
+        self.battery = Battery()
+
+class Bank:
+    def __init__(self):
+        self.balance = 0
+        
+class Ressource:
+    '''
+    Guide pour le système de ressource
+    ID: 
+        001 : Pierre
+        002 : Bois
+        003 : Eau
+    '''
+    def __init__(self):
+        self.id = randint(1,3)
+        self.position = Position()
+        #prix à ajouter
+
+agent_1 = Robot()
