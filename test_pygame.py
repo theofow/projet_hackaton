78f563b7317212babfda2afca7c3a539dc0786ea
theophile <theophile.rabut@edu.devinci.fr>
2023-04-03 10:49:22 +0200

    edit + passage sur pygame

--- test_pygame.py
+++ test_pygame.py
@@ -1,31 +1,26 @@
-import tkinter
-from tkinter import ttk
-from projet import *
+import pygame
+pygame.init()
 
+#génération de la fenêtre
+pygame.display.set_caption("Pojet : ROBOT")
+screen = pygame.display.set_mode((1080, 720))
+background = pygame.image.load("map.jpg")
+background = pygame.transform.scale(background, (1080, 720))
 
-robots = [Robot(Position()) for i in range(5)]
+running = True
 
+while running :
+    
 
-root = tkinter.Tk()
-root.geometry("600x600")
+    screen.blit(background, (0, 0))
 
+    pygame.display.flip()
 
-for i in range(20):
-    for j in range(20):
-        label = ttk.Label(root, text="", borderwidth=2, relief="groove", width=3, font=("Helvetica", 12))
-        label.grid(column=i, row=j)
+    for event in pygame.event.get():
 
+        if event.type == pygame.QUIT:
+            running = False
+            pygame.quit()
+            print("Fermeture du jeu...")
 
-robot_colors = ["red", "green", "blue", "purple", "orange"]
-
-
-def update_robots():
-    for i, robot in enumerate(robots):
-        x, y = robot.position.coo()
-        label = root.grid_slaves(row=y, column=x)[0]
-        label.configure(background=robot_colors[i % len(robot_colors)])
-
-root.after(100, update_robots)
-
-root.mainloop()
 
