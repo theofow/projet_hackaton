c13587a19199a90a3571db30593b3c28338493aa
theophile <theophile.rabut@edu.devinci.fr>
2023-04-03 11:02:06 +0200

    commentaires sur game_class

--- game_class.py
+++ game_class.py
@@ -1,4 +1,7 @@
-#Ajout des lib
+#############################################################
+#                         IMPORTS                           #
+#############################################################
+
 from random import randint
 
 
@@ -7,6 +10,10 @@ from random import randint
 #############################################################
 
 
+
+# Position définit la position d'un objet sur la map.
+# Elle dispose de plusieurs méthodes permettant le déplacement par exemple.
+# Tous les objets sont régis par cette classe.
 
 class Position:
     def __init__(self):
@@ -28,16 +35,22 @@ class Position:
         self.x = randint(0,20)
         self.y = randint(0,20)
     
-    
+
+# Battery définit la batterie des robots et diminue avec les déplacement.
+
 class Battery:
     def __init__(self):
         self.percent = 100
-        self.level = 1
+        self.max_percent = 100
+
+# Stock correspond au stockage des robots (leur max et leur contenu).
 
 class Stock:
     def __init__(self):
         self.max = 10
         self.content = []
+
+# Robot représente un robot au départ
 
 class Robot:
     def __init__(self, position=None):
@@ -45,11 +58,16 @@ class Robot:
         self.battery = Battery()
         self.stock = Stock()
 
+# Bank est la classe définissant le score obtenu par les robots
 
 class Bank:
     def __init__(self):
         self.balance = 0
         
+# Ressource est la classe qui définit chaque ressource. Certaines apparaissent
+# aléatoirement tandis que d'autre sont fixe. Leur taux d'apparition est
+# variable en fonction de leur valeur.
+
 class Ressource:
     '''
     Guide pour le système de ressource
@@ -57,10 +75,14 @@ class Ressource:
         001 : Pierre
         002 : Bois
         003 : Eau
+        004 : Or
     '''
     def __init__(self):
-        self.id = randint(1,3)
+        self.id = randint(1,4)
         self.position = Position()
         #prix à ajouter
 
+
+
+# création d'un agent (test)
 agent_1 = Robot()
