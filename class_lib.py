ff3bfe4bac9276f2f693868e2d75f77ea83cfe66
Glyx <florian.aubre97@gmail.com>
2023-04-16 23:09:04 +0200

    définition de X et Y dans la classe grille

--- class_lib.py
+++ class_lib.py
@@ -6,7 +6,6 @@ from random import randint
 import pygame
 
 
-
 #############################################################
 #                         CLASSES                           #
 #############################################################
@@ -17,12 +16,11 @@ import pygame
 # dans la fenêtre.
 
 class Grille:
+    X = 0
+    Y = 0
+
     def __init__(self):
-        self.X = 0
-        self.Y = 0
-
-
-
+        pass
 
 
 # Position définit la position d'un objet sur la map.
@@ -31,25 +29,25 @@ class Grille:
 
 class Position(Grille):
     def __init__(self):
-        self.x = 54*Grille.X
-        self.y = 48*Grille.Y
-        self.xy = (self.x,self.y)
+        self.x = 54 * Grille.X
+        self.y = 48 * Grille.Y
+        self.xy = (self.x, self.y)
+
     def coo(self):
         return self.x, self.y
-    
+
     def deplace(self, ax, ay):
         self.x = self.x + ax
         self.y = self.y + ay
-        self.xy = (self.x,self.y)
-    
+        self.xy = (self.x, self.y)
+
     def reset_pos(self):
         self.x = 0
         self.y = 0
-        
-    def random_pos(self):
-        self.x = randint(0,20)
-        self.y = randint(0,15)
 
+    def random_pos(self):
+        self.x = randint(0, 20)
+        self.y = randint(0, 15)
 
 
 # Battery définit la batterie des robots et diminue avec les déplacement.
@@ -59,12 +57,14 @@ class Battery:
         self.percent = 100
         self.max_percent = 100
 
+
 # Stock correspond au stockage des robots (leur max et leur contenu).
 
 class Stock:
     def __init__(self):
         self.max = 10
         self.content = []
+
 
 # La Team change le point d'apparition du robot
 
@@ -78,9 +78,7 @@ class Robot(pygame.sprite.Sprite):
         self.battery = Battery()
         self.stock = Stock()
         self.image = pygame.transform.scale(pygame.image.load("robot_sprite.png"), (52, 46))
-        #self.rect = self.image.get_rect()
-        
-
+        # self.rect = self.image.get_rect()
 
 
 # Bank est la classe définissant le score obtenu par les robots
@@ -88,7 +86,8 @@ class Robot(pygame.sprite.Sprite):
 class Bank:
     def __init__(self):
         self.balance = 0
-        
+
+
 # Ressource est la classe qui définit chaque ressource. Certaines apparaissent
 # aléatoirement tandis que d'autre sont fixe. Leur taux d'apparition est
 # variable en fonction de leur valeur.
@@ -102,11 +101,11 @@ class Ressource:
         003 : Eau
         004 : Or
     '''
-    def __init__(self):
-        self.id = randint(1,4)
-        self.position = Position()
-        #prix à ajouter
 
+    def __init__(self):
+        self.id = randint(1, 4)
+        self.position = Position()
+        # prix à ajouter
 
 
 # création d'un agent (test)
@@ -115,6 +114,3 @@ class Ressource:
 class Game:
     def __init__(self):
         self.robot = Robot()
-
-
-
