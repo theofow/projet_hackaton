2b4daf3ad40b23c0dc3fd114467b942aa76067d4
Glyx <35071511+Glyx@users.noreply.github.com>
2023-04-17 04:02:59 +0200

    eh vsy

--- main.py
+++ main.py
@@ -1,20 +1,12 @@
-#############################################################
-#                         IMPORTS                           #
-#############################################################
-
 import pygame
 from class_lib import *
 from pygame.locals import *
 from pygame import mixer
-
-
-#############################################################
-#               GENERATION DE LA FENETRE                    #
-#############################################################
+from Deplacement_robot import deplace
 
 pygame.init()
 
-pygame.display.set_caption("Pojet : ROBOT")
+pygame.display.set_caption("Projet : ROBOT")
 screen = pygame.display.set_mode((1080, 720))
 background = pygame.image.load("annexes/map.jpg")
 background = pygame.transform.scale(background, (1080, 720))
@@ -23,109 +15,43 @@ mixer.init()
 mixer.music.load('annexes/music.ogg')
 mixer.music.play()
 
-
-
-#############################################################
-#               MISE EN PLACE DES ELEMENTS                  #
-#############################################################
-
-# Robots
 robot1 = Robot()
 robot2 = Robot()
+robot2.position.random_pos()
+robot1.position.random_pos()
 
-robot2.position.set_pos(18,2)
-
-teste = Ressource()
-teste.position.random_pos()
-
-tic = 0
-
-
-#############################################################
-#                        LANCEMENT                          #
-#############################################################
+ressources = []
+for i in range(5):
+    ressources.append(Ressource())
+    ressources[i].position.random_pos()
 
 running = True
 
-while running :
-    
-
+while running:
     screen.blit(background, (0, 0))
 
+    for ressource in ressources:
+        screen.blit(ressource.image, ressource.position.xy)
 
-    screen.blit(robot2.image, robot2.position.xy)
+        # si un robot est sur la ressource
+        if robot1.position.coo() == ressource.position.coo():
+            robot1.score += ressource.value
+            ressource.position.random_pos()
+        elif robot2.position.coo() == ressource.position.coo():
+            robot2.score += ressource.value
+            ressource.position.random_pos()
+
+    # déplacement des robots
+    deplace(robot1)
+    deplace(robot2)
 
     screen.blit(robot1.image, robot1.position.xy)
-    
-    screen.blit(teste.image, teste.position.xy)
-   
-
-   
-    
+    screen.blit(robot2.image, robot2.position.xy)
 
     pygame.display.flip()
 
-#############################################################
-#                          EVENT                            #
-#############################################################
-
     for event in pygame.event.get():
-        # Cette boucle représente les évènements càd les éléments qui donnent 
-        # vie au jeu et qui permette l'intéraction avec l'utilisateur
-
-
-        # FERMETURE DU PROGRAMME
-
         if event.type == pygame.QUIT:
             running = False
             pygame.quit()
             print("Fermeture du programme...")
-
-        # DEPLACEMENT DU ROBOT
-
-        elif event.type == pygame.KEYDOWN:
-            if event.key == pygame.K_d:
-                robot2.position.deplace(1,0)
-                robot2.switch_sprite("right")
-                robot2.battery.lower
-                tic +=1
-            elif event.key == pygame.K_q:
-                robot2.position.deplace(-1,0)
-                robot2.switch_sprite("left")
-                robot2.battery.lower
-                tic +=1
-            elif event.key == pygame.K_s:
-                robot2.position.deplace(0,1)
-                robot2.switch_sprite("down")
-                robot2.battery.lower
-                tic +=1
-            elif event.key == pygame.K_z:
-                robot2.position.deplace(0,-1)
-                robot2.switch_sprite("up")
-                robot2.battery.lower
-                tic +=1
-            elif event.key == pygame.K_RIGHT:
-                robot1.position.deplace(1,0)
-                robot1.switch_sprite("right")
-                robot1.battery.lower
-                tic +=1
-            elif event.key == pygame.K_LEFT:
-                robot1.position.deplace(-1,0)
-                robot1.switch_sprite("left")
-                robot1.battery.lower
-                tic +=1
-            elif event.key == pygame.K_DOWN:
-                robot1.position.deplace(0,1)
-                robot1.switch_sprite("down")
-                robot1.battery.lower
-                tic +=1
-            elif event.key == pygame.K_UP:
-                 robot1.position.deplace(0,-1)
-                 robot1.switch_sprite("up")
-                 robot1.battery.lower
-                 tic +=1
-        
-    #if robot1.battery == 0 or robot2.battery == 0:
-     #   running = False
-      #  pygame.quit()
-       # print("Fermeture du programme...")
--- readme.md
+++ readme.md
@@ -10,13 +10,13 @@ Théophile RABUT / __theofow__
 
 Tahir Aboukhassib / __tahirabou__
 
-Geoffrey Aubrée / __Glyx__
+ Geoffrey Aubrée / __Glyx__
  
-Matisse Marchand / __matisse08__
+ Matisse Marchand / __jsp__
 
 
 
-### Approche du projet
+ ### Approche du projet
 
 Notre projet consiste en la création d'une simulation de robots autonomes en utilisant le language __python__. Ce language est très utile pour la manipulation de données.
 
@@ -24,12 +24,12 @@ Dans le cadre de notre projet, nous allons nous servir de ce language afin de co
 
 
 ### Robots
-Chaque agent a pour mission de récolter des ressources afin d'amasser du score. Il dispose d'une batterie limitée. Il s'arrête donc lorsque sa batterie est vide.
+Chaque agent a pour mission de récolter des ressources et de les ramener à sa station. Il dispose d'une batterie limitée ainsi qu'un espace de stockage limité. Il revient donc à sa station lorsque son stockage est plein ou que sa batterie est presque vide.
 
 Voici quelques information qui résument très bien le comportement des robots :
 - Un robot emprunte toujours le chemin le plus optimal vers son target
-- Un robot cible en priorité les ressources proches
-- Un robot s'arrête quand sa batterie ne lui permet pas de continuer sa mission
+- Un robot cible en priorité les ressources rentables, c'est à dire proches et chère
+- Un robot rentre immédiatement en station quand sa batterie ne lui permet pas de continuer sa mission
 
 ### Ressources
 
@@ -39,26 +39,25 @@ Les données concernant les ressources sont :
 - Leur volume
 - Leur ID
 
+>Liste des ressources:
+>
+>ID: 001 - Pierre
+>
+>ID: 002 - Bois
+>
+>ID: 003 - Eau
+>
+>ID: 004 - Or
+
+Chaque ressource apparait aléatoirement, ce qui crée donc un comportement aléatoire chez les robots qui vont cibler les ressources à portée.
+
 Classement des ressources de la moins chère à la plus chère : 
 
 __Pierre < Bois < Eau < Or.__
 
->Liste des ressources:
->
->ID: 001 - Pierre | **1pt**
->
->ID: 002 - Bois | **2pts**
->
->ID: 003 - Eau | **3pts**
->
->ID: 004 - Or | **5pts**
-
-Chaque ressource apparait aléatoirement, ce qui crée donc un comportement aléatoire chez les robots qui vont cibler les ressources à portée.
-
-
 ## Fonctionnement
 
-Lors du lancement du programme, les robots partent de leur station en quête de ressources à récupérer. Une déchargé, chaque robot s'arrête et les scores tranchent sur l'agent gagnant.
+Lors du lancement du programme, les robots partent de leur station en quête de ressources à récupérer. Une fois plein, ou déchargé, chaque robot ira à sa station et ajoutera donc du score à son nom. Le premier agent qui atteint le score max met fin à la simulation.
 
 ## Précisions 
 
@@ -90,24 +89,13 @@ Pour commencer, il faut cloner le repository github sur l'ordinateur utilisé.
 
 Entrez la clé __HTTP__ du repository.
 
+Note:
 
-## Installation de <img src="annexes/pygame_logo.png" alt="drawing" width="70"/>
+1.Le système de ressource est ouvert à tous les joueurs.
 
-- Intaller pygame sur vscode
+2.Les joueurs peuvent utiliser des stratégies différentes pour collecter des ressources.
 
-`pip install pygame`
+3.Les règles du jeu sont soumises à des modifications ultérieures en fonction des commentaires des joueurs.
 
-<img src="annexes/screen_install_pygame.png" alt="drawing" width="500"/> 
-
-- Utiliser Pygame
-
-Pour utiliser des fonctions de pygame il suffit d'utiliser `import pygame`.
-
-Cependant il faut noter qu'il sera parfois nécéssaire d'intégrer d'autres fonctions plus spécifiques.
-
-La documentation est accessible [ici](https://www.pygame.org/docs/) pour plus de précisions.
-
-
-# Lancer le projet
-
-Pour lancer le projet, utiliser `main.py` uniquement.
+4.Les joueurs peuvent contacter l'administrateur pour toute question ou préoccupation.
+Merci de participer à ce projet!
